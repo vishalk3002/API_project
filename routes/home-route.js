815ac50da2ee729e1e9b9a82841f39a7da1c2241
Vishal Kumar <147840321+vishalk3002@users.noreply.github.com>
2025-12-09 19:04:04 +0530

    Update home-route.js

--- routes/home-route.js
+++ routes/home-route.js
@@ -4,11 +4,12 @@ const authMiddleware = require("../middleware/auth-middleware");
 
 router.get("/welcome", authMiddleware, (req, res) => {
   //adding one more layer of protection so that only admin can access this page
-const { _id, username, role } = req.user;
+
+  const { _id, username, role } = req.userInfo;
   res.json({
     message: "Welcome to the home page",
     user: {
-      _id: userId,
+      _id,
       username,
       role,
     },
