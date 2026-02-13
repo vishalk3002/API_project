8a34f2fe041f9fe3cca04fdca050c55cb8ac7ad8
Vishal Kumar <147840321+vishalk3002@users.noreply.github.com>
2026-02-13 20:57:26 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
+ try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+
+    isConnected = conn.connections[0].readyState;
     console.log("MongoDB connected successfully!");
+    
   } catch (e) {
-    console.error("MongoDB connection failed");
-    process.exit(1);
+    console.error("MongoDB connection failed:", e.message);
+
+    // ❌ NEVER DO process.exit on serverless
+    throw new Error("DB connection failed");
   }
 };
 
